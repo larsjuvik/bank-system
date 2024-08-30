f5b24225168213d7ade65f59cd6407b58288b035
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-08-30 17:31:33 +0200

    added simple and insecure login system for demo purposes

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Components;
 using WebApp.Security;
-using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,8 +13,8 @@ builder.Services.AddRazorComponents()
 builder.Services.AddDbContext<BankContext>(options => options.UseInMemoryDatabase("BankSystemDemo"));
 
 // Authentication
+builder.Services.AddScoped<DemoAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider, DemoAuthenticationStateProvider>();
-builder.Services.AddAuthorizationCore();
 
 var app = builder.Build();
 
--- WebApp/Security/DemoAuthenticationStateProvider.cs
+++ WebApp/Security/DemoAuthenticationStateProvider.cs
@@ -9,6 +9,11 @@ namespace WebApp.Security
     {
         private readonly ProtectedSessionStorage localStorage;
         private ClaimsPrincipal _currentUser = new(new ClaimsIdentity());
+        struct User
+        {
+            public string Username { get; set; }
+            public string Role { get; set; }
+        }
 
         public DemoAuthenticationStateProvider(ProtectedSessionStorage localStorage)
         {
@@ -18,10 +23,17 @@ namespace WebApp.Security
         public async Task Initialize()
         {
             // Get the authentication state from local storage
-            var state = await localStorage.GetAsync<ClaimsPrincipal?>("authenticationState");
-            if (state.Value != null && state.Value.Identity != null && state.Value.Identity.IsAuthenticated)
+            var state = await localStorage.GetAsync<User?>("authenticationState");
+            if (state.Value != null)
             {
-                _currentUser = state.Value;
+                var identity = new ClaimsIdentity(
+            [
+                new Claim(ClaimTypes.Name, state.Value.Value.Username),
+                new Claim(ClaimTypes.Role, state.Value.Value.Role)
+            ], "demo");
+
+                _currentUser = new ClaimsPrincipal(identity);
+
                 NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(_currentUser)));
             }
         }
@@ -35,7 +47,6 @@ namespace WebApp.Security
         {
             return Task.FromResult(new AuthenticationState(_currentUser));
         }
-
 
         public async Task<bool> Login(string username, string password)
         {
@@ -51,17 +62,20 @@ namespace WebApp.Security
             [
                 new Claim(ClaimTypes.Name, username),
                 new Claim(ClaimTypes.Role, "User")
-            ], "User");
+            ], "demo");
 
             // Create a new ClaimsPrincipal with the identity
             var user = new ClaimsPrincipal(identity);
 
             // Store the authentication state in local storage
-            await localStorage.SetAsync("authenticationState", user);
+            await localStorage.SetAsync("authenticationState", new
+            {
+                Username = username,
+                Role = "User"
+            });
 
             // Notify the authentication state has changed
             NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
-
             return true;
         }
 
