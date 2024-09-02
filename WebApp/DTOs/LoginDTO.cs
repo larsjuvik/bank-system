2f351960d5f98144e151dbbb71f9109f56ee9a05
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-09-02 15:46:17 +0200

    refactor DTO to separate file

---
+++ WebApp/DTOs/LoginDTO.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+public class LoginDTO
+{
+    [Required]
+    [MinLength(5)]
+    [MaxLength(20)]
+    public string? Username { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [MinLength(8)]
+    [MaxLength(100)]
+    public string? Password { get; set; }
+}
