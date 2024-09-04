132109815ad26c3a46550d9e90132d48dd677b47
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-09-04 22:14:49 +0200

    removed password changing from DTO

--- WebApp/DTOs/EditUserDTO.cs
+++ WebApp/DTOs/EditUserDTO.cs
@@ -10,20 +10,6 @@ public class EditUserDTO
     public string Username { get; set; } = string.Empty;
 
     [Required]
-    [DataType(DataType.Password)]
-    [MinLength(8)]
-    [MaxLength(100)]
-    public string Password { get; set; } = string.Empty;
-
-    [Required]
-    [DataType(DataType.Password)]
-    [Display(Name = "Repeat Password")]
-    [MinLength(8)]
-    [MaxLength(100)]
-    [Compare(nameof(Password), ErrorMessage = "The passwords do not match.")]
-    public string RepeatPassword { get; set; } = string.Empty;
-
-    [Required]
     [DataType(DataType.Date)]
     [Display(Name = "Date of Birth")]
     [MinimumAge(18, ErrorMessage = "You must be at least 18 years old.")]
