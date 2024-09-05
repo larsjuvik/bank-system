3af6555b8c69a9bd099beb9ee8de985a7c51262d
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-09-05 21:19:00 +0200

    added functionality for seeding with middle name

--- Data/BankContext.cs
+++ Data/BankContext.cs
@@ -44,23 +44,54 @@ public class BankContext(DbContextOptions<BankContext> options) : DbContext(opti
             .OnDelete(DeleteBehavior.Restrict);
     }
 
+    /// <summary>
+    /// Creates a set of known users, and then a lot of test users.
+    /// </summary>
     private static void SeedDatabaseWithTestData(ModelBuilder modelBuilder)
     {
-        (int userIdIndex, int bankAccountIndex, int transactionIndex) = CreateKnownUsers(modelBuilder);
-        CreateTestUsers(modelBuilder, GetRandomNames(100), userIdIndex+1, bankAccountIndex+1, transactionIndex+1);
+        var (userIdIndex, bankAccountIndex, transactionIndex) = CreateKnownUsers(modelBuilder);
+        CreateTestData(modelBuilder, GetRandomNames(100), userIdIndex+1, bankAccountIndex+1, transactionIndex+1);
     }
 
     private static List<string> GetRandomNames(int count)
     {
-        List<string> firstNames = new List<string>
+        var maleFirstNames = new List<string>
         {
-            "James", "Mary", "John", "Patricia", "Robert", "Jennifer", "Michael", "Linda", "William", "Elizabeth",
-            "David", "Barbara", "Richard", "Susan", "Joseph", "Jessica", "Thomas", "Sarah", "Charles", "Karen",
-            "Christopher", "Nancy", "Daniel", "Lisa", "Matthew", "Betty", "Anthony", "Margaret", "Mark", "Sandra",
-            "Donald", "Ashley", "Steven", "Kimberly", "Paul", "Emily", "Andrew", "Donna", "Joshua", "Michelle",
-            "Kenneth", "Carol", "Kevin", "Amanda", "Brian", "Dorothy", "George", "Melissa", "Edward", "Deborah"
+            "James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles",
+            "Christopher", "Daniel", "Matthew", "Anthony", "Mark", "Donald", "Steven", "Paul", "Andrew", "Joshua",
+            "Kenneth", "Kevin", "Brian", "George", "Edward", "Ronald", "Timothy", "Jason", "Jeffrey", "Ryan",
+            "Jacob", "Gary", "Nicholas", "Eric", "Jonathan", "Stephen", "Larry", "Scott", "Frank", "Brandon",
+            "Benjamin", "Gregory", "Samuel", "Patrick", "Alexander", "Jack", "Dennis", "Jerry", "Tyler", "Aaron"
         };
-        List<string> lastNames = new List<string>
+
+        var maleMiddleNames = new List<string>
+        {
+            "James", "Michael", "John", "David", "William", "Joseph", "Thomas", "Charles", "Edward", "Alexander",
+            "Scott", "Paul", "Christopher", "Daniel", "Anthony", "Ray", "Patrick", "Lee", "Andrew", "Benjamin",
+            "Richard", "Brian", "Allen", "George", "Robert", "Ryan", "Stephen", "Matthew", "Timothy", "Alexander",
+            "Samuel", "Mark", "Joseph", "Henry", "Jacob", "Frank", "Peter", "Nicholas", "Gregory", "Lawrence",
+            "Phillip", "Martin", "Nathan", "Jameson", "Elliot", "Aaron", "Francis", "Adam", "Raymond", "Louis"
+        };
+        
+        var femaleFirstNames = new List<string>
+        {
+            "Mary", "Patricia", "Jennifer", "Linda", "Elizabeth", "Barbara", "Susan", "Jessica", "Sarah", "Karen",
+            "Nancy", "Margaret", "Sandra", "Ashley", "Kimberly", "Emily", "Donna", "Michelle", "Dorothy", "Carol",
+            "Amanda", "Melissa", "Deborah", "Stephanie", "Rebecca", "Sharon", "Laura", "Cynthia", "Kathleen", "Amy",
+            "Angela", "Shirley", "Brenda", "Pamela", "Emma", "Nicole", "Helen", "Samantha", "Katherine", "Christine",
+            "Debra", "Rachel", "Carolyn", "Janet", "Catherine", "Maria", "Heather", "Diane", "Virginia", "Julie"
+        };
+
+        var femaleMiddleNames = new List<string>
+        {
+            "Marie", "Ann", "Elizabeth", "Grace", "Renee", "Lynn", "Jean", "Rose", "Elaine", "Nicole",
+            "Michelle", "Marie", "Jane", "Louise", "Faith", "Claire", "Hope", "Joy", "Diana", "Marie",
+            "Evelyn", "Christine", "Leigh", "Marie", "Mae", "June", "Lee", "Elaine", "Pearl", "Sue",
+            "Marie", "Victoria", "Faye", "Irene", "Marie", "Elaine", "Claire", "Marie", "Michelle", "Isabelle",
+            "Ann", "Marie", "Christine", "Joyce", "Sophia", "Marie", "Marie", "Taylor", "Jean", "Marie"
+        };
+
+        var lastNames = new List<string>
         {
             "Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez",
             "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson", "Thomas", "Taylor", "Moore", "Jackson", "Martin",
@@ -71,19 +102,77 @@ public class BankContext(DbContextOptions<BankContext> options) : DbContext(opti
 
         var names = new List<string>();
         var random = new Random();
-        for (int i = 0; i < count; i++)
+        var femaleNamesCount = count / 2;
+        var maleNamesCount = count / 2;
+        
+        // If the number is odd, randomly assign the extra name to male or female
+        if (count % 2 != 0)
         {
-            var firstname = firstNames[random.Next(0, firstNames.Count)];
+            if (random.Next(2) == 0) // 0 or 1
+            {
+                femaleNamesCount++;
+            }
+            else
+            {
+                maleNamesCount++;
+            }
+        }
+        
+        for (var i = 0; i < femaleNamesCount; i++)
+        {
+            var firstname = femaleFirstNames[random.Next(0, femaleFirstNames.Count)];
+            var middleName = string.Empty;
+            if (GetRandomBoolean())
+            {
+                middleName = femaleMiddleNames[random.Next(0, femaleMiddleNames.Count)];
+            }
+
             var lastName = lastNames[random.Next(0, lastNames.Count)];
-            names.Add(firstname + " " + lastName);
+            if (string.IsNullOrEmpty(middleName))
+            {
+                names.Add(firstname + " " + lastName);
+            }
+            else
+            {
+                names.Add(firstname + " " + middleName + " " + lastName);
+            }
+        }
+        
+        for (var i = 0; i < maleNamesCount; i++)
+        {
+            var firstname = maleFirstNames[random.Next(0, maleFirstNames.Count)];
+            var middleName = string.Empty;
+            if (GetRandomBoolean())
+            {
+                middleName = maleMiddleNames[random.Next(0, maleMiddleNames.Count)];
+            }
+
+            var lastName = lastNames[random.Next(0, lastNames.Count)];
+            if (string.IsNullOrEmpty(middleName))
+            {
+                names.Add(firstname + " " + lastName);
+            }
+            else
+            {
+                names.Add(firstname + " " + middleName + " " + lastName);
+            }
         }
         
         return names;
     }
 
-    private static void CreateTestUsers(ModelBuilder modelBuilder, List<string> names, int userIndexStart, int bankAccountIndexStart, int transactionIndexStart)
+    /// <summary>
+    /// Creates randomized test users, bank accounts and transactions.
+    /// </summary>
+    /// <param name="modelBuilder">The model builder to add models to</param>
+    /// <param name="names">The names of the users - the length decides number of users added</param>
+    /// <param name="userIndexStart">The start index of user id</param>
+    /// <param name="bankAccountIndexStart">The start index of bank account id</param>
+    /// <param name="transactionIndexStart">The start index of transaction id</param>
+    private static void CreateTestData(ModelBuilder modelBuilder, List<string> names, int userIndexStart, int bankAccountIndexStart, int transactionIndexStart)
     {
         var random = new Random();
+        
         var userIndex = userIndexStart;
         var users = new List<User>();
         foreach (var name in names)
