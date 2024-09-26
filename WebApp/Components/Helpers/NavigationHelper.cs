4da7ec2089b28d373e6b026037dfb24c9cd82689
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-09-26 02:07:39 +0200

    code improvements and refactor

--- WebApp/Components/Helpers/NavigationHelper.cs
+++ WebApp/Components/Helpers/NavigationHelper.cs
@@ -5,12 +5,22 @@ namespace WebApp.Components.Helpers;
 public static class NavigationHelper
 {
     /// <summary>
-    /// Redirects to returnUrl if set, else returns to home screen. Performs force load and replace.
+    /// Redirects to returnUrl if set, else returns to home screen.
+    /// Performs force load and history stack replace.
     /// </summary>
     public static void ForceRedirectReturnUrlOrHome(NavigationManager navigation)
     {
         var uri = new Uri(navigation.Uri);
         var returnUrl = System.Web.HttpUtility.ParseQueryString(uri.Query).Get("returnUrl");
         navigation.NavigateTo(returnUrl ?? "/", true, true);
+    }
+
+    /// <summary>
+    /// Redirects to home screen.
+    /// Performs force load and history stack replace.
+    /// </summary>
+    public static void ForceRedirectHome(NavigationManager navigation)
+    {
+        navigation.NavigateTo("/", true, true);
     }
 }
