1718732b41c4567dbdc6ed385296320bda44507f
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-10-22 18:29:44 +0200

    refactoring

--- Data/Repositories/TransactionRepository.cs
+++ Data/Repositories/TransactionRepository.cs
@@ -8,15 +8,15 @@ public class TransactionRepository(BankContext context)
     /// <summary>
     /// Get transactions by user id, where the user is the sender or receiver of the transaction
     /// </summary>
-    /// <param name="id">The id of the user</param>
+    /// <param name="userId">The id of the user</param>
     /// <returns>A queryable transactions object</returns>
-    public IQueryable<Transaction> GetAllTransactionsByUserIdAsync(int id)
+    public IQueryable<Transaction> GetAllTransactionsByUserIdAsync(int userId)
     {
         return context.Transactions
             .Include(t => t.From)
             .ThenInclude(b=>b.Owner)
             .Include(t => t.To)
             .ThenInclude(b=>b.Owner)
-            .Where(t => t.To != null && t.From != null && (t.To.Owner.Id == id || t.From.Owner.Id == id));
+            .Where(t => t.To != null && t.From != null && (t.To.Owner.Id == userId || t.From.Owner.Id == userId));
     }
 }
