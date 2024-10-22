cb4593b1cb075ac812b528fafd1fd4346666c82f
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-10-22 18:29:35 +0200

    fixed bug where wrong id was used to get transactions

--- Data/Repositories/TransactionRepository.cs
+++ Data/Repositories/TransactionRepository.cs
@@ -14,35 +14,9 @@ public class TransactionRepository(BankContext context)
     {
         return context.Transactions
             .Include(t => t.From)
+            .ThenInclude(b=>b.Owner)
             .Include(t => t.To)
-            .Where(t => t.To != null && t.To.Id == id);
-    }
-
-    /// <summary>
-    /// Get transactions by user id, where the user is the sender of the transaction
-    /// </summary>
-    /// <param name="id">The id of the user</param>
-    /// <returns>A list of transactions</returns>
-    public async Task<List<Transaction>> GetFromTransactionsByUserIdAsync(int id)
-    {
-        return await context.Transactions
-            .Include(t => t.From)
-            .Include(t => t.To)
-            .Where(t => t.From.Id == id)
-            .ToListAsync();
-    }
-
-    /// <summary>
-    /// Get transactions by user id, where the user is the receiver of the transaction
-    /// </summary>
-    /// <param name="id">The id of the user</param>
-    /// <returns>A list of transactions</returns>
-    public async Task<List<Transaction>> GetToTransactionsByUserIdAsync(int id)
-    {
-        return await context.Transactions
-            .Include(t => t.From)
-            .Include(t => t.To)
-            .Where(t => t.To.Id == id)
-            .ToListAsync();
+            .ThenInclude(b=>b.Owner)
+            .Where(t => t.To != null && t.From != null && (t.To.Owner.Id == id || t.From.Owner.Id == id));
     }
 }
